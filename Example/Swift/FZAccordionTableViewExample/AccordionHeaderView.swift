7c7108a6e267f090877d8807885bd74096b86485
Kris Gaidis <kgaidis@gmail.com>
2015-10-05 11:41:29 -0400

    Added first version of a Swift Example.

---
+++ Example/Swift/FZAccordionTableViewExample/AccordionHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  AccordionHeaderView.swift
+//  FZAccordionTableViewExample
+//
+//  Created by Krisjanis Gaidis on 10/5/15.
+//  Copyright © 2015 Fuzz. All rights reserved.
+//
+
+import UIKit
+
+class AccordionHeaderView: FZAccordionTableViewHeaderView {
+    static let kDefaultAccordionHeaderViewHeight: CGFloat = 44.0;
+    static let kAccordionHeaderViewReuseIdentifier = "AccordionHeaderViewReuseIdentifier";
+}
