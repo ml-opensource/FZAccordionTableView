e3451dbbedb5445233b55b43a74b012af47272b5
Kris Gaidis <kgaidis@gmail.com>
2016-10-23 15:54:20 -0400

    Added an Swift example that shows the usage of dynamic-height cells.

---
+++ Example/Swift/Dynamic-Sizing/FZAccordionTableViewExample/Cell.swift
@@ -0,0 +1,15 @@
+//
+//  Cell.swift
+//  FZAccordionTableViewExample
+//
+//  Created by Krisjanis Gaidis on 10/23/16.
+//  Copyright © 2016 Fuzz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Cell: UITableViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+}
---
+++ Example/Swift/Dynamic-Sizing/FZAccordionTableViewExample/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  FZAccordionTableViewExample
+//
+//  Created by Krisjanis Gaidis on 10/5/15.
+//  Copyright © 2015 Fuzz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    static fileprivate let kTableViewCellReuseIdentifier = "Cell"
+    @IBOutlet fileprivate weak var tableView: FZAccordionTableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.allowMultipleSectionsOpen = true
+        tableView.register(UINib(nibName: "AccordionHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: AccordionHeaderView.kAccordionHeaderViewReuseIdentifier)
+    }
+}
+
+// MARK: - Extra Overrides - 
+
+extension ViewController {
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
+}
+
+// MARK: - Helpers
+
+func randomString() -> String {
+    let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    let numberOfLetters = UInt32(letters.length)
+    let stringLength = arc4random_uniform(200)
+    var randomString = ""
+    for _ in 0 ..< stringLength {
+        let rand = max(1, arc4random_uniform(numberOfLetters))
+        var nextChar = letters.character(at: Int(rand))
+        randomString += NSString(characters: &nextChar, length: 1) as String
+    }
+    return randomString
+}
+
+// MARK: - <UITableViewDataSource> / <UITableViewDelegate> -
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return AccordionHeaderView.kDefaultAccordionHeaderViewHeight
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.tableView(tableView, heightForRowAt: indexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        return self.tableView(tableView, heightForHeaderInSection:section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ViewController.kTableViewCellReuseIdentifier, for: indexPath) as! Cell
+        cell.titleLabel.text = randomString()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return tableView.dequeueReusableHeaderFooterView(withIdentifier: AccordionHeaderView.kAccordionHeaderViewReuseIdentifier)
+    }
+}
+
+// MARK: - <FZAccordionTableViewDelegate> -
+
+extension ViewController : FZAccordionTableViewDelegate {
+
+    func tableView(_ tableView: FZAccordionTableView, willOpenSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
+        
+    }
+    
+    func tableView(_ tableView: FZAccordionTableView, didOpenSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
+        
+    }
+    
+    func tableView(_ tableView: FZAccordionTableView, willCloseSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
+        
+    }
+    
+    func tableView(_ tableView: FZAccordionTableView, didCloseSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
+        
+    }
+    
+    func tableView(_ tableView: FZAccordionTableView, canInteractWithHeaderAtSection section: Int) -> Bool {
+        return true
+    }
+}
