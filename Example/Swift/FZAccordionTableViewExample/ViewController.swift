b14ff8a799c5a725b6340223a98ee5cbce979655
Kris Gaidis <kgaidis@gmail.com>
2016-10-23 15:33:23 -0400

    Converted Swift example to Swift 3.0

--- Example/Swift/FZAccordionTableViewExample/ViewController.swift
+++ Example/Swift/FZAccordionTableViewExample/ViewController.swift
@@ -9,21 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    static private let kTableViewCellReuseIdentifier = "TableViewCellReuseIdentifier"
-    @IBOutlet private weak var tableView: FZAccordionTableView!
+    static fileprivate let kTableViewCellReuseIdentifier = "TableViewCellReuseIdentifier"
+    @IBOutlet fileprivate weak var tableView: FZAccordionTableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.allowMultipleSectionsOpen = true
-        tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: ViewController.kTableViewCellReuseIdentifier)
-        tableView.registerNib(UINib(nibName: "AccordionHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: AccordionHeaderView.kAccordionHeaderViewReuseIdentifier)
+        tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: ViewController.kTableViewCellReuseIdentifier)
+        tableView.register(UINib(nibName: "AccordionHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: AccordionHeaderView.kAccordionHeaderViewReuseIdentifier)
     }
 }
 
 // MARK: - Extra Overrides - 
 
 extension ViewController {
-    override func prefersStatusBarHidden() -> Bool {
+    override var prefersStatusBarHidden : Bool {
         return true
     }
 }
@@ -32,38 +32,38 @@ extension ViewController {
 
 extension ViewController : UITableViewDelegate, UITableViewDataSource {
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 5;
     }
     
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 44.0;
     }
     
-    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return AccordionHeaderView.kDefaultAccordionHeaderViewHeight
     }
     
-    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return self.tableView(tableView, heightForRowAtIndexPath: indexPath)
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.tableView(tableView, heightForRowAt: indexPath)
     }
     
-    func tableView(tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
         return self.tableView(tableView, heightForHeaderInSection:section)
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(ViewController.kTableViewCellReuseIdentifier, forIndexPath: indexPath)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ViewController.kTableViewCellReuseIdentifier, for: indexPath)
         cell.textLabel!.text = "Cell"
         return cell
     }
     
-    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return tableView.dequeueReusableHeaderFooterViewWithIdentifier(AccordionHeaderView.kAccordionHeaderViewReuseIdentifier)
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return tableView.dequeueReusableHeaderFooterView(withIdentifier: AccordionHeaderView.kAccordionHeaderViewReuseIdentifier)
     }
 }
 
@@ -71,23 +71,23 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource {
 
 extension ViewController : FZAccordionTableViewDelegate {
 
-    func tableView(tableView: FZAccordionTableView, willOpenSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
+    func tableView(_ tableView: FZAccordionTableView, willOpenSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
         
     }
     
-    func tableView(tableView: FZAccordionTableView, didOpenSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
+    func tableView(_ tableView: FZAccordionTableView, didOpenSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
         
     }
     
-    func tableView(tableView: FZAccordionTableView, willCloseSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
+    func tableView(_ tableView: FZAccordionTableView, willCloseSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
         
     }
     
-    func tableView(tableView: FZAccordionTableView, didCloseSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
+    func tableView(_ tableView: FZAccordionTableView, didCloseSection section: Int, withHeader header: UITableViewHeaderFooterView?) {
         
     }
     
-    func tableView(tableView: FZAccordionTableView, canInteractWithHeaderAtSection section: Int) -> Bool {
+    func tableView(_ tableView: FZAccordionTableView, canInteractWithHeaderAtSection section: Int) -> Bool {
         return true
     }
 }
